8c7a9ae892a9c2a3ffca8ad8da9a92e8c67cd566
JakubMizera <kubamiz53@gmail.com>
2022-10-18 16:21:52 +0200

    moved files to diff folders

--- projekt-zaliczeniowy/src/index.tsx
+++ projekt-zaliczeniowy/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../App';
-import { Menu } from '../components/common/Menu';
-import { GlobalStyle } from '../style/global';
+import App from './components/common/App';
+import { Menu } from './components/common/Menu';
+import { GlobalStyle } from './style/global';
 
 
 
